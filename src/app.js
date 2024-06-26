239d5852395dc74b41c9f401d33ee2fe5c06c683
Carlos Noguera <124005636+CarlosNC-DEV@users.noreply.github.com>
2024-06-26 15:11:08 -0500

    Limit none and 94px margin in document

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const getPdf = async (html) => {
   let browser = null;
@@ -37,10 +37,10 @@ const getPdf = async (html) => {
     format: 'A4',
     printBackground: true,
     margin: {
-      top: '30px',
-      right: '30px',
-      bottom: '30px',
-      left: '30px'
+      top: '94px',
+      right: '94px',
+      bottom: '94px',
+      left: '94px'
     }
   });
   await browser.close();
