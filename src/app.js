89ee341d66d20564acc17e7b3c14e26002c0165e
Carlos Noguera <124005636+CarlosNC-DEV@users.noreply.github.com>
2024-06-26 12:35:39 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,16 @@ const getPdf = async (html) => {
   }
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: 'networkidle0' });
-  const pdfBuffer = await page.pdf();
+  const pdfBuffer = await page.pdf({
+    format: 'A4',
+    printBackground: true,
+    margin: {
+      top: '20px',
+      right: '20px',
+      bottom: '20px',
+      left: '20px'
+    }
+  });
   await browser.close();
   return pdfBuffer;
 };
